6009f149cd410518dba5ea0bfa5013b1d6158f83
MICHAEL FRANGOS <mike@mikefrangos.net>
2013-11-13 21:18:52 +0100

    first version

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//
+// PHASE: BOOTSTRAP
+//
+define('MMVC_INSTALL_PATH', dirname(__FILE__));
+define('MMVC_SITE_PATH', MMVC_INSTALL_PATH . '/site');
+
+require(MMVC_INSTALL_PATH.'/src/CMmvc/bootstrap.php');
+
+$mm = CMmvc::Instance();
+
+//
+// PHASE: FRONTCONTROLLER ROUTE
+//
+$mm->FrontControllerRoute();
+
+
+//
+// PHASE: THEME ENGINE RENDER
+//
+$mm->ThemeEngineRender();
---
+++ themes/core/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Helpers for the template file.
+*/
+$mm->data['header'] = '<h1>Header: Mmvc</h1>';
+// $mm->data['main']   = '<p>Main: Now with a theme engine, Not much more to report for now.</p>';
+$mm->data['footer'] = '<p>Footer: &copy; Mmvc by Mike Frangos</p>';
+
+
+
